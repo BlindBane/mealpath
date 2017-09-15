bc80070e21f4e16b9238fd5c21b0f7df6fbafa1b
Aidan Bane <aidanfbane@gmail.com>
2017-09-15 08:18:38 -0700

    (feat) Added Yelp API search template and token to .env

---
+++ server/utils/yelpApi.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const yelp = require('yelp-fusion');
+
+const searchRequest = {
+  term: 'Four Barrel Coffee',
+  location: 'san francisco, ca',
+  limit: 20,
+  open_now: true
+};
+
+const yelpSearch = () => {
+  const client = yelp.client(process.env.YELP_TOKEN);
+
+  client.search(searchRequest).then(response => {
+    const firstResult = response.jsonBody.businesses[0];
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+    console.log(prettyJson);
+  });
+};
+
+module.exports = yelpSearch;
